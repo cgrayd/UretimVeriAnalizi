62eff515128d1a420aa99d8a41ac0d18b77ecb69
cgrayd <cagriaydin23@gmail.com>
2025-05-13 18:50:06 +0300

    Üretilen verilerin analizini yapar
    Bu uygulama, üretim verilerini tablo ve grafiklerle gösterir. Makine ve tarih filtrelemesi yapılabilir, veriler dinamik olarak güncellenir.

---
+++ UretimVeriAnalizi/UretimVeriAnalizi/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace UretimVeriAnalizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerBas = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerBitis = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Makine Seç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Baslangıç Tarihi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Bitiş Tarihi";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(143, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // dateTimePickerBas
+            // 
+            this.dateTimePickerBas.Location = new System.Drawing.Point(143, 95);
+            this.dateTimePickerBas.Name = "dateTimePickerBas";
+            this.dateTimePickerBas.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerBas.TabIndex = 2;
+            // 
+            // dateTimePickerBitis
+            // 
+            this.dateTimePickerBitis.Location = new System.Drawing.Point(143, 149);
+            this.dateTimePickerBitis.Name = "dateTimePickerBitis";
+            this.dateTimePickerBitis.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerBitis.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 271);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 265);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(661, 113);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(561, 483);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 818);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePickerBitis);
+            this.Controls.Add(this.dateTimePickerBas);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBas;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBitis;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UretimVeriAnalizi/UretimVeriAnalizi/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace UretimVeriAnalizi
+{
+    public partial class Form1 : Form
+    {
+        int gunSayisi = 8;
+        int makineSayisi = 4;
+        Random random = new Random();
+        DataTable originalDataTable = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            gridViewBaslat();
+            VeriTablosuBasla();
+            VerileriEkle();
+            comboBoxMakinelerreEkle();
+        }
+
+        private void gridViewBaslat()
+        {
+            dataGridView1.Columns.Add("Makine", "Makine");
+            dataGridView1.Columns.Add("Tarih", "Tarih");
+            dataGridView1.Columns.Add("HedefMiktar", "Hedef Miktar");
+            dataGridView1.Columns.Add("UretilenMiktar", "Üretilen Miktar");
+
+            dataGridView1.Columns["Makine"].Width = 100;
+            dataGridView1.Columns["Tarih"].Width = 100;
+            dataGridView1.Columns["HedefMiktar"].Width = 100;
+            dataGridView1.Columns["UretilenMiktar"].Width = 100;
+        }
+
+        private void VeriTablosuBasla()
+        {
+            originalDataTable.Columns.Add("Makine");
+            originalDataTable.Columns.Add("Tarih");
+            originalDataTable.Columns.Add("HedefMiktar");
+            originalDataTable.Columns.Add("UretilenMiktar");
+        }
+
+        private void VerileriEkle()
+        {
+            DateTime BasTar = new DateTime(2024, 6, 1);
+
+            for (int Gun = 0; Gun < gunSayisi; Gun++)
+            {
+                DateTime SimdikiTar = BasTar.AddDays(Gun);
+                for (int Makine = 1; Makine <= makineSayisi; Makine++)
+                {
+                    string makineAdi = "Makine" + Makine;
+                    string tarih = SimdikiTar.ToString("dd/MM/yyyy");
+                    int hedefMiktar = random.Next(100, 301);
+                    int uretilenMiktar = random.Next(100, 301);
+
+                    dataGridView1.Rows.Add(makineAdi, tarih, hedefMiktar, uretilenMiktar);
+                    originalDataTable.Rows.Add(makineAdi, tarih, hedefMiktar, uretilenMiktar);
+                }
+            }
+        }
+
+        private void comboBoxMakinelerreEkle()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Hepsi");
+            for (int Makine = 1; Makine <= makineSayisi; Makine++)
+            {
+                string makineAdi = "Makine" + Makine;
+                comboBox1.Items.Add(makineAdi);
+            }
+
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FilterDataGridView();
+            UpdateChart();
+        }
+
+        private void FilterDataGridView()
+        {
+            string SeciliMakine = comboBox1.SelectedItem.ToString();
+            DateTime BasTar = dateTimePickerBas.Value;
+            DateTime BitTar = dateTimePickerBitis.Value;
+
+            var filtre = originalDataTable.AsEnumerable().Where(row =>
+            {
+                DateTime Satir = DateTime.ParseExact(row.Field<string>("Tarih"), "dd/MM/yyyy", null);
+                bool TarihlerAra = Satir >= BasTar && Satir <= BitTar;
+                bool EslesenMakine = SeciliMakine == "Hepsi" || row.Field<string>("Makine") == SeciliMakine;
+
+                return TarihlerAra && EslesenMakine;
+            });
+
+            DataTable filtrelenmisSatir;
+            if (filtre.Any())
+            {
+                filtrelenmisSatir = filtre.CopyToDataTable();
+            }
+            else
+            {
+                filtrelenmisSatir = originalDataTable.Clone(); // Boş ama aynı yapıda tablo
+            }
+
+            dataGridView1.Rows.Clear();
+
+            foreach (DataRow VeriSatiri in filtrelenmisSatir.Rows)
+            {
+                dataGridView1.Rows.Add(VeriSatiri.ItemArray);
+            }
+        }
+
+        private void UpdateChart()
+        {
+            chart1.Series.Clear();
+            var hedefMiktarSeries = new Series("Hedef Miktar");
+            var uretilenMiktarSeries = new Series("Üretilen Miktar");
+
+            hedefMiktarSeries.ChartType = SeriesChartType.Column;
+            uretilenMiktarSeries.ChartType = SeriesChartType.Column;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["Tarih"].Value != null)
+                {
+                    DateTime date = DateTime.ParseExact(row.Cells["Tarih"].Value.ToString(), "dd/MM/yyyy", null);
+                    int hedefMiktar = int.Parse(row.Cells["HedefMiktar"].Value.ToString());
+                    int uretilenMiktar = int.Parse(row.Cells["UretilenMiktar"].Value.ToString());
+
+                    hedefMiktarSeries.Points.AddXY(date, hedefMiktar);
+                    uretilenMiktarSeries.Points.AddXY(date, uretilenMiktar);
+                }
+            }
+
+            chart1.Series.Add(hedefMiktarSeries);
+            chart1.Series.Add(uretilenMiktarSeries);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
